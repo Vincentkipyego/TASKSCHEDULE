7413a53703e0e28bfec0a049cf6f1b3f5fb6bfc9
vincent <vincentkipyego@gmail.com>
2022-09-09 18:42:28 +0300

    flatlist done

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View , ScrollView,  FlatList} from 'react-native';
 
 export default function App() {
+  const [newtext, setNewtext] = useState('');
+  const[displaygoals, setDisplaygoals] = useState([]);
+ 
+function goalInputHandler (keyintext){ 
+   setNewtext(keyintext)
+
+}
+
+function addNewHandler(){
+setDisplaygoals(allthegoals => [...allthegoals,{ text: newtext, id:Math.random().toString()} ] )
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+     <View style={styles.inputcontainer}>
+    <TextInput  style={styles.textin} placeholder='Your Course Goals' onChangeText={goalInputHandler}/>
+    <Button  title='Add GOAL ' onPress={addNewHandler}/>
+     </View>
+
+     <View style= {styles.goalsdisplay} >
+     <FlatList data={displaygoals} 
+     renderItem={(itemData)=> { 
+       return(
+         <View  style={styles.displaygoals}>
+         <Text >{itemData.item.text}</Text>
+         </View>
+       );
+    }} 
+  keyExtractor= {(item, index)=> {return item.id;}}
+    alwaysBounceVertical= {true}/>
+     
+
+    
+     </View>
+      </View>
   );
 }
 
@@ -14,7 +44,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal:20,
   },
+  inputcontainer :
+  {
+    flex:1,
+    flexDirection:'row',
+    justifyContent:"space-between",
+    alignItems:"center",
+    marginBottom: 24,
+    borderBottomColor: "grey",
+    borderBottomWidth :1,
+  
+  },
+  textin :{
+    backgroundColor:"white",
+    borderColor:'grey',
+    borderWidth:1,
+    marginRight: 8,
+    width: "70%",
+    padding:8,
+   
+ },
+ goalsdisplay:{
+   flex:6,
+  
+ },
+ displaygoals :{
+  margin :6,
+  borderRadius: 10,
+  padding:12,
+  borderWidth:1,
+  backgroundColor:"white",
+ },
 });
